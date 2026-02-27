ca289fd393c3d91e72708ce0462b37da14c0a1ac
ssiidduu-2005 <siddukarthik2005@gmail.com>
2026-02-27 16:36:40 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-// "use client"
-import React from "react";
+"use client"
+import React, { useState } from "react";
 // import { easeIn, easeOut, motion } from "framer-motion";
 // const fadeUp = {
 //   hidden: { opacity: 0, y: -30 },
@@ -10,9 +10,16 @@ import React from "react";
 //   },
 // };
 const Navbar = () => {
+
+  const [data,setdata]=useState();
+
+  const menu=()=>{
+    setdata(!data);
+  }
+  console.log(data);
   return (
-    <div className="logo_font fixed z-10 mt-1 flex h-auto w-[90%] items-center justify-between rounded-xl bg-[#f0f0f0] px-4 py-2 text-3xl font-bold text-[#212121] sm:mt-3 sm:w-[90%] sm:py-2.5 lg:mt-5 lg:max-w-fit lg:gap-20 lg:py-3 xl:gap-60">
-      <div className="flex items-center gap-1 py-0.5">
+    <div className="logo_font fixed z-10 mt-1 flex h-auto w-[90%] items-center justify-between rounded-xl bg-[#f0f0f0]  py-2 text-3xl font-bold text-[#212121] sm:mt-3 sm:w-[90%] sm:py-2.5 lg:mt-5 lg:max-w-fit lg:gap-20 lg:py-3 xl:gap-60">
+      <div className="flex items-center gap-1 py-0.5 px-4">
         <span className="flex-1">
           {" "}
           <svg
@@ -34,7 +41,7 @@ const Navbar = () => {
         <span>Dehalt</span>
       </div>
 
-      <div className="flex justify-center rounded-sm bg-[#ff6600] p-1.5 lg:hidden">
+      <div className="flex justify-center rounded-sm bg-[#ff6600] p-1.5 lg:hidden mr-4" onClick={menu}>
         <svg
           width="20"
           height="20"
@@ -51,6 +58,10 @@ const Navbar = () => {
           />
         </svg>
       </div>
+      
+
+
+
       <div className="h-[full hidden w-full items-end lg:flex lg:gap-15">
         <div className="text-2xl font-medium">Services</div>
         <div className="text-2xl font-medium">Cases</div>
@@ -59,7 +70,7 @@ const Navbar = () => {
       <div className="hidden w-full items-center gap-2.5 p-0.5 font-bold lg:flex">
         <div className="text-2xl">Talk to us</div>
 
-        <div className="h-full rounded-sm bg-[#ff6600] p-2">
+        <div className="h-full rounded-sm bg-[#ff6600] p-2 mr-2">
           <svg
             width="20"
             height="20"
@@ -77,7 +88,40 @@ const Navbar = () => {
           </svg>
         </div>
       </div>
+            <div className={`h-140 w-full lg:hidden  absolute ${data ? "block" : "hidden"} mt-150`}>
+          <div className=' h-42 md:w-auto bg-white mt-3   rounded-lg pt-1 pl-1 pr-1 '>
+          <div className='h-12 w-auto bg-gray-100 rounded-md pl-3 pt-3'><h1 className='text-[20px] font-bold'>Services</h1></div>
+          <div className='h-12 w-auto bg-gray-100 rounded-md mt-2 pl-3 pt-3'><h1 className='text-[20px]  font-bold'>Cases</h1></div>
+          <div className='h-12 w-auto bg-gray-100 rounded-md mt-2 pl-3 pt-3'><h1 className='text-[20px]  font-bold'>About</h1></div>
+          </div>
+          <div className='h-13 w-auto bg-white mt-1 rounded-md pt-1  flex  pr-2 '> 
+            <div className='h-11 w-full  flex justify-end items-center text-lg gap-2'>
+              <span>Talk to us</span>
+              <div className=" rounded-sm bg-[#ff6600] p-2">
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+              <path
+                d="M7 17L17 7M17 7H9M17 7V15"
+                stroke="black"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              </svg>
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
+  
+
+  
+
   );
 };
 
