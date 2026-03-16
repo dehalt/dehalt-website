11c2ada4889e9a4b82377cf59fd50b196b610667
ssiidduu-2005 <dev@dehalt.com>
2026-03-16 17:51:02 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,56 @@
+"use client";
 import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='w-full py-10 bg-[#121212] px-40 flex justify-between text-white'>
-        <span className='text-sm text-gray-400'>&copy; Copyrights 2026</span>
-        <span>Terms and Conditaions</span>
-        <span>Privacy policy</span>
+    
+  <div className="w-full bg-[#121212] py-8 px-6">
+  <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-8 text-gray-400 text-sm font-semibold">
+
+        <div className="text-gray-600 text-xs order-4 md:order-1">
+                © 2026 Dapper
+              </div>
+    
+    <div className="flex gap-6 order-3 md:order-2">
+      <span>Privacy Policy</span>
+      <span>Terms and Conditions</span>
     </div>
+
+
+       <div className="flex gap-6 order-1 md:order-3">
+      <span>Design by Dylan</span>
+      <span>Code by Dennis</span>
+    </div>
+           <div className="flex gap-4 order-2 md:order-4">
+            <div>
+          <svg
+         className="w-15 h-15 md:w-12 md:h-12 rounded-sm"
+            fill="#000000"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="-143 145 512 512"
+            xmlSpace="preserve"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+
+            <g id="SVGRepo_iconCarrier">
+              <path d="M329,145h-432c-22.1,0-40,17.9-40,40v432c0,22.1,17.9,40,40,40h432c22.1,0,40-17.9,40-40V185C369,162.9,351.1,145,329,145z M41.4,508.1H-8.5V348.4h49.9V508.1z M15.1,328.4h-0.4c-18.1,0-29.8-12.2-29.8-27.7c0-15.8,12.1-27.7,30.5-27.7 c18.4,0,29.7,11.9,30.1,27.7C45.6,316.1,33.9,328.4,15.1,328.4z M241,508.1h-56.6v-82.6c0-21.6-8.8-36.4-28.3-36.4 c-14.9,0-23.2,10-27,19.6c-1.4,3.4-1.2,8.2-1.2,13.1v86.3H71.8c0,0,0.7-146.4,0-159.7h56.1v25.1c3.3-11,21.2-26.6,49.8-26.6 c35.5,0,63.3,23,63.3,72.4V508.1z"></path>
+            </g>
+          </svg>
+        </div>
+          </div>
+          
+    </div>
+    </div>
+
   );
 };
 
