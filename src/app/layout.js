eb368cf6e7e285d8001635bd926abd70a2fc8c2a
Dehalt <dev@dehalt.com>
2026-03-18 09:23:13 +0530

    google tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,10 +28,21 @@ export const metadata = {
   },
 };
 
- 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18017065552">
+        </script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-18017065552');
+        </script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
