af45615a9c67fdec37c0d6bed4ce1943609ee5cf
Dehalt <dev@dehalt.com>
2026-03-23 12:00:20 +0530

    smooth scroll

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import SmoothScroll from '@/components/SmoothScroll'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -52,6 +54,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SmoothScroll />
         {children}
       </body>
     </html>
---
+++ src/components/SmoothScroll.js
@@ -0,0 +1,27 @@
+'use client'
+
+import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
+
+export default function SmoothScroll() {
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 2, // increase = slower scroll
+            smoothWheel: true,
+            smoothTouch: false, // keep mobile normal unless you want chaos
+        })
+
+        function raf(time) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+
+        requestAnimationFrame(raf)
+
+        return () => {
+            lenis.destroy()
+        }
+    }, [])
+
+    return null
+}
